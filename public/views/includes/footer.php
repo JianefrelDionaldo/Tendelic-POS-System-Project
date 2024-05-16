c3db9bb34712e6f66e98a142b4ae20cbf9bafb6b
JianefrelDionaldo <dionaldojian01@gmail.com>
2024-05-17 03:53:50 +0800

    First commit

---
+++ public/views/includes/footer.php
@@ -0,0 +1,4 @@
+  <script src="/../tendelic/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- YOU CAN ALSO USE The Script Below -->
+  <!-- <script src="/../tendelic/public/assets/js/bootstrap.bundle.min.js"></script> -->
+</html>
---
+++ public/views/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $title; ?></title>
+  <link rel="stylesheet" href="/../tendelic/dist/main.css">
+  <!-- Open Graph -->
+  <link rel="canonical" href="https://www.tendelic.com/">
+  <meta property="og:title" content="Tendélic - It combines tender and delicacy, suggesting a place where tender steaks are treated as delicate culinary treasures.">
+  <meta name="description" content="Tendélic: - Where tender steak are crafted into delicate culinary treasures.">
+  <meta property="og:local" content="en_US">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.tendelic.com">
+  <meta property="og:site_name" content="Tendelic">
+</head>
